0d98ba174087fd9f061677d0ddc8510bdb61c410
Abdul Wagab Tamimul Ansari <65432634+abdulwagab@users.noreply.github.com>
2023-03-20 08:51:17 +0530

    Add files via upload

---
+++ Python-Vector-CANoe-master/Sample.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+#.Data:2018/5/19
+
+import CANoe
+import time
+
+app = CANoe.CANoe() #定义CANoe为app
+
+app.open_simulation("test.cfg") #导入某个CANoe congif
+
+app.start_Measurement() #启动CANoe
+
+var_from_namespace = app.get_all_SysVar("mfl") #获取namespace下的所有系统变量
+
+print(app.get_SysVar("mfl","vol_plus")) #获取系统变量的值
+
+app.set_SysVar("mfl","vol_plus",1)  #写入系统变量的值
+
+app.stop_Measurement() #停止CANoe
